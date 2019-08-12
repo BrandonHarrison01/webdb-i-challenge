c0e41b9d5dd92f37fd592986469e7570cb917898
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-12 15:16:06 -0600

    update and delete account endpoints are functional

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.json());
 
 
-// UPDATE
+// CREATE
 
 server.post('/api/accounts', (req, res) => {
     const post = req.body;
@@ -17,12 +17,53 @@ server.post('/api/accounts', (req, res) => {
         .catch(error => res.status(500).json({ error: 'there was an error posting to account' }))
 })
 
+
 // READ
 
 server.get('/api/accounts', (req, res) => {
     db.select('*').from('accounts')
         .then(result => res.status(200).json(result))
         .catch(error => res.status(500).json({ error: 'there was an error' }))
+})
+
+
+// UPDATE
+
+server.put('/api/accounts/:id', (req, res) => {
+    const changes = req.body;
+
+    db('accounts')
+        .where('id', '=', req.params.id)
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ error: 'account not found' })
+            }
+        })
+    .catch(error => {
+        res.status(500).json({ error: 'there was an error updating account' })
+    })
+})
+
+
+// DELETE
+
+server.delete('/api/accounts/:id', (req, res) => {
+    db('accounts')
+        .where('id', '=', req.params.id)
+        .del()
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ error: 'account not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'there was an error deleting account'})
+        })
 })
 
 module.exports = server;
