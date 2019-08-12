dcd34d5016b60af793a2d267276e537cc1cd5bfd
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-12 14:59:45 -0600

    create and read accounts endpoints are complete

--- server.js
+++ server.js
@@ -6,4 +6,23 @@ const server = express();
 
 server.use(express.json());
 
+
+// UPDATE
+
+server.post('/api/accounts', (req, res) => {
+    const post = req.body;
+    db('accounts')
+        .insert(post, 'id')
+        .then(post => res.status(200).json(post))
+        .catch(error => res.status(500).json({ error: 'there was an error posting to account' }))
+})
+
+// READ
+
+server.get('/api/accounts', (req, res) => {
+    db.select('*').from('accounts')
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(500).json({ error: 'there was an error' }))
+})
+
 module.exports = server;
